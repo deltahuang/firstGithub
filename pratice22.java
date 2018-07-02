0ae4bbf103aaa3ab9af31fc3ca83a97755f49e57
deltahuang <40678943+deltahuang@users.noreply.github.com>
2018-07-02 18:47:29 +0800

    变动了
    一点点

--- pratice22.java
+++ pratice22.java
@@ -12,14 +12,14 @@ public class pratice22 {
 		i=i-5;
 	}*/
 	
-	/*do {
+	do {
 			System.out.println(i);
 		i=i-5;
-	} while (i>=0);*/
+	} while (i>=0);
 	
-	for ( i = 100; i >=0; i=i-5) {
+/*	for ( i = 100; i >=0; i=i-5) {
 		System.out.println(i);
-	}
+	}*/
 	
 	}
 
