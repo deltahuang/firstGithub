e8fd8b8487679e054d05cc4efe0443b5ea6c9124
deltahuang <40678943+deltahuang@users.noreply.github.com>
2018-07-02 18:43:48 +0800

    Add files via upload

---
+++ pratice22.java
@@ -0,0 +1,26 @@
+package chapter3;
+
+public class pratice22 {
+
+	public static void main(String[] args) {
+	int i=100;
+	
+/*	while (i>=0) {
+		
+	
+		System.out.println(i);
+		i=i-5;
+	}*/
+	
+	/*do {
+			System.out.println(i);
+		i=i-5;
+	} while (i>=0);*/
+	
+	for ( i = 100; i >=0; i=i-5) {
+		System.out.println(i);
+	}
+	
+	}
+
+}
